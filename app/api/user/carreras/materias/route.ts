dfb36ac20ce81d89b22db691b173605f82ec0fbc
Victor <99147005+victor-POL@users.noreply.github.com>
2025-08-06 17:30:46 -0300

    fix: nombre campo rta api

--- app/api/user/carreras/materias/route.ts
+++ app/api/user/carreras/materias/route.ts
@@ -49,7 +49,7 @@ export async function GET(request: NextRequest) {
     return NextResponse.json({
       success: true,
       data: {
-        hhistorial: historialDB,
+        historial: historialDB,
         estadisticas: estadisticasDB,
       },
     })
